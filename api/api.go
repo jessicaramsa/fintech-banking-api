81b2a2a286f43da5574d05efdef75cff054ac5ef
Jéssica Ramírez <jessicaramsa@gmail.com>
2020-06-10 21:35:46 -0500

    Register a new user

--- api/api.go
+++ api/api.go
@@ -17,6 +17,12 @@ type Login struct {
 	Password string
 }
 
+type Register struct {
+	Username string
+	Email    string
+	Password string
+}
+
 type ErrResponse struct {
 	Message string
 }
@@ -39,9 +45,28 @@ func login(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func register(w http.ResponseWriter, r *http.Request) {
+	body, err := ioutil.ReadAll(r.Body)
+	helpers.HandleErr(err)
+
+	var formattedBody Register
+	err = json.Unmarshal(body, &formattedBody)
+	helpers.HandleErr(err)
+	register := users.Register(formattedBody.Username, formattedBody.Email, formattedBody.Password)
+
+	if register["message"] == "OK" {
+		resp := register
+		json.NewEncoder(w).Encode(resp)
+	} else {
+		resp := ErrResponse{Message: "Wrong username or password"}
+		json.NewEncoder(w).Encode(resp)
+	}
+}
+
 func StartApi() {
 	router := mux.NewRouter()
 	router.HandleFunc("/login", login).Methods("POST")
+	router.HandleFunc("/register", register).Methods("POST")
 	fmt.Println("Server is listening on port: 8888")
 	log.Fatal(http.ListenAndServe(":8888", router))
 }
--- helpers/helpers.go
+++ helpers/helpers.go
@@ -1,6 +1,9 @@
 package helpers
 
 import (
+	"regexp"
+
+	"github.com/jessicaramsa/fintech-banking-app/interfaces"
 	"github.com/jinzhu/gorm"
 	_ "github.com/lib/pq"
 	"golang.org/x/crypto/bcrypt"
@@ -22,4 +25,27 @@ func ConnectDB() *gorm.DB {
 	db, err := gorm.Open("postgres", "host=127.0.0.1 port=5432 user=postgres dbname=fintech_banking_app password=' ' sslmode=disable")
 	HandleErr(err)
 	return db
+}
+
+func Validation(values []interfaces.Validation) bool {
+	username := regexp.MustCompile(`^([A-Za-z0-9]{5,})+$`)
+	email := regexp.MustCompile(`^[A-Za-z0-9]+[@]+[A-Za-z0-9]+[.]+[A-Za-z]+$`)
+
+	for i := 0; i < len(values); i++ {
+		switch values[i].Valid {
+		case "username":
+			if !username.MatchString(values[i].Value) {
+				return false
+			}
+		case "email":
+			if !email.MatchString(values[i].Value) {
+				return false
+			}
+		case "password":
+			if len(values[i].Value) < 5 {
+				return false
+			}
+		}
+	}
+	return true
 }
